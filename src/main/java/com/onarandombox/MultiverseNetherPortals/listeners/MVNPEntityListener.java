88eaebe063c1ae5c26f85e35bdf77e2ce6165410
Han Programer <30620423+Hanprogramer@users.noreply.github.com>
2023-06-08 10:27:20 +0700

    Disabled the warning

--- src/main/java/com/onarandombox/MultiverseNetherPortals/listeners/MVNPEntityListener.java
+++ src/main/java/com/onarandombox/MultiverseNetherPortals/listeners/MVNPEntityListener.java
@@ -234,28 +234,28 @@ public class MVNPEntityListener implements Listener {
         Location toLocation = getLocation(p, currentLocation, type, currentWorld, linkedWorld);
 
         if (toLocation == null) {
-            if (this.shootPlayer(p, eventLocation.getBlock(), type)) {
-                eventRecord.removeFromRecord(type, p.getUniqueId());
-            }
-
-            if (currentWorld.equalsIgnoreCase(linkedWorld)) {
-                if (this.plugin.isSendingDisabledPortalMessage()) {
-                    if (type == PortalType.ENDER) {
-                        this.messaging.sendMessage(p, "End Portals have been disabled in this world!", false);
-                    } else {
-                        this.messaging.sendMessage(p, "Nether Portals have been disabled in this world!", false);
-                    }
-                }
-            } else {
-                if (this.plugin.isSendingNoDestinationMessage()) {
-                    this.messaging.sendMessage(p, "This portal goes nowhere!", false);
-                    if (type == PortalType.ENDER) {
-                        this.messaging.sendMessage(p, "No specific end world has been linked to this world and '" + this.nameChecker.getEndName(currentWorld) + "' is not a world.", true);
-                    } else {
-                        this.messaging.sendMessage(p, "No specific nether world has been linked to this world and '" + this.nameChecker.getNetherName(currentWorld) + "' is not a world.", true);
-                    }
-                }
-            }
+//            if (this.shootPlayer(p, eventLocation.getBlock(), type)) {
+//                eventRecord.removeFromRecord(type, p.getUniqueId());
+//            }
+//
+//            if (currentWorld.equalsIgnoreCase(linkedWorld)) {
+//                if (this.plugin.isSendingDisabledPortalMessage()) {
+//                    if (type == PortalType.ENDER) {
+//                        this.messaging.sendMessage(p, "End Portals have been disabled in this world!", false);
+//                    } else {
+//                        this.messaging.sendMessage(p, "Nether Portals have been disabled in this world!", false);
+//                    }
+//                }
+//            } else {
+//                if (this.plugin.isSendingNoDestinationMessage()) {
+//                    this.messaging.sendMessage(p, "This portal goes nowhere!", false);
+//                    if (type == PortalType.ENDER) {
+//                        this.messaging.sendMessage(p, "No specific end world has been linked to this world and '" + this.nameChecker.getEndName(currentWorld) + "' is not a world.", true);
+//                    } else {
+//                        this.messaging.sendMessage(p, "No specific nether world has been linked to this world and '" + this.nameChecker.getNetherName(currentWorld) + "' is not a world.", true);
+//                    }
+//                }
+//            }
 
             return;
         }
